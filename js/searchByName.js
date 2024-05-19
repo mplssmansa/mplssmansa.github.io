87794bd8d8d8e8ad601f81d50617f03f7a96c1a1
Artamananda <artamananda@gmail.com>
2024-05-19 17:47:00 +0700

    add search by name page

---
+++ js/searchByName.js
@@ -0,0 +1,63 @@
+import { fetchData } from "./fetchData.js";
+
+let allData = [];
+const loadingScreen = document.getElementById("loading-screen");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    loadingScreen.style.display = "flex";
+    const dataRes = await fetchData();
+    allData = dataRes.dataPeserta;
+    displayData(allData);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    tableBody.innerHTML = '<tr><td colspan="2">Error loading data</td></tr>';
+  } finally {
+    loadingScreen.style.display = "none";
+  }
+});
+
+function displayData(data) {
+  const tableBody = document.getElementById("tableBody");
+  tableBody.innerHTML = "";
+  if (data?.length > 0) {
+    data?.forEach((item) => {
+      const row = document.createElement("tr");
+      row.className = "bg-white";
+
+      const nameCell = document.createElement("td");
+      nameCell.className = "py-2 px-4";
+      nameCell.textContent = item.nama;
+
+      const downloadCell = document.createElement("td");
+      downloadCell.className = "py-2 px-4";
+      downloadCell.textContent = item.nopendaftaran;
+
+      row.appendChild(nameCell);
+      row.appendChild(downloadCell);
+      tableBody.appendChild(row);
+    });
+  } else {
+    const row = document.createElement("tr");
+    const noResultCell = document.createElement("td");
+    noResultCell.setAttribute("colspan", 2);
+    const noResultSpan = document.createElement("span");
+    noResultCell.className = "py-2 px-4 text-center italic";
+    noResultSpan.textContent = "No result found";
+    noResultCell.appendChild(noResultSpan);
+    row.appendChild(noResultCell);
+    tableBody.appendChild(row);
+  }
+}
+
+window.filterData = function () {
+  const searchTerm = document
+    .getElementById("search-input")
+    .value.toLowerCase();
+  const regex = new RegExp(searchTerm, "i");
+  const filteredData = allData.filter((item) => {
+    const name = item.nama || "";
+    return regex.test(name);
+  });
+  displayData(filteredData);
+};
